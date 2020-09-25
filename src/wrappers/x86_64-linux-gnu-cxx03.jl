7256c84acb08e7757c8a8ff1289edf6c5ca0646c
jlbuild <juliabuildbot@gmail.com>
2020-09-25 19:42:21 +0000

    tblis_jll build 1.2.0+1

--- src/wrappers/x86_64-linux-gnu-cxx03.jl
+++ src/wrappers/x86_64-linux-gnu-cxx03.jl
@@ -10,7 +10,7 @@ LIBPATH_env = "LD_LIBRARY_PATH"
 LIBPATH_default = ""
 
 # Relative path to `tblis`
-const tblis_splitpath = ["lib", "libtblis.so.0"]
+const tblis_splitpath = ["lib", "libtblis.so"]
 
 # This will be filled out by __init__() for all products, as it must be done at runtime
 tblis_path = ""
@@ -24,7 +24,7 @@ const tblis = "libtblis.so.0"
 
 
 # Relative path to `tci`
-const tci_splitpath = ["lib", "libtci.so.0"]
+const tci_splitpath = ["lib", "libtci.so"]
 
 # This will be filled out by __init__() for all products, as it must be done at runtime
 tci_path = ""
@@ -37,16 +37,18 @@ tci_handle = C_NULL
 const tci = "libtci.so.0"
 
 
+# Inform that the wrapper is available for this platform
+wrapper_available = true
+
 """
 Open all libraries
 """
 function __init__()
-    global artifact_dir = abspath(artifact"tblis")
+    # This either calls `@artifact_str()`, or returns a constant string if we're overridden.
+    global artifact_dir = find_artifact_dir()
 
-    # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
+    # Initialize PATH and LIBPATH environment variable listings
     # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
     # then append them to our own.
     foreach(p -> append!(PATH_list, p), (Hwloc_jll.PATH_list, CompilerSupportLibraries_jll.PATH_list,))
@@ -56,26 +58,21 @@ function __init__()
 
     # Manually `dlopen()` this right now so that future invocations
     # of `ccall` with its `SONAME` will find this path immediately.
-    global tblis_handle = dlopen(tblis_path)
+    global tblis_handle = dlopen(tblis_path, RTLD_LAZY | RTLD_DEEPBIND)
     push!(LIBPATH_list, dirname(tblis_path))
 
     global tci_path = normpath(joinpath(artifact_dir, tci_splitpath...))
 
     # Manually `dlopen()` this right now so that future invocations
     # of `ccall` with its `SONAME` will find this path immediately.
-    global tci_handle = dlopen(tci_path)
+    global tci_handle = dlopen(tci_path, RTLD_LAZY | RTLD_DEEPBIND)
     push!(LIBPATH_list, dirname(tci_path))
 
     # Filter out duplicate and empty entries in our PATH and LIBPATH entries
     filter!(!isempty, unique!(PATH_list))
     filter!(!isempty, unique!(LIBPATH_list))
     global PATH = join(PATH_list, ':')
-    global LIBPATH = join(LIBPATH_list, ':')
+    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
 
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    
 end  # __init__()
-
